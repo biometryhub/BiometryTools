9a866ad2f9536ed93c33c9071077c5057590c857
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2020-10-02 10:09:23 +0930

    Updated packages

--- R/transfer_packages.R
+++ R/transfer_packages.R
@@ -1,18 +1,21 @@
 #' Easily transfer a list of all installed packages from one machine to another
 #'
 #' @param library The location of the library on the current machine to copy.
-#' @param output
-#' @param expiry Expiry for online file store.
-#' @param quiet
+#' @param output One of `online` (the default), `local` or `github`. See Details for more information.
+#' @param expiry Expiry for online file store in days. Weeks can be given with `w`, or months with `m`. Default is 7 days. Will be ignored if `output` is not `online`.
+#' @param quiet Don't display output. Defaults to `FALSE`.
 #' @param format
+#'
+#' @details If `output` is `online`, the resulting list of currently installed packages is stored on [https://file.io](https://file.io) for the time specified in `expiry`.
+#' If `output` is `local`
+#'
 #'
 #' @importFrom httr POST
 #'
 #' @return
 #' @export
 #'
-#' @examples
-transfer_packages <- function(library = .libPaths()[1], output = "online", expiry="14d", format = "list", quiet = F) {
+transfer_packages <- function(library = .libPaths()[1], output = "online", expiry="7d", format = "list", quiet = F) {
     # Get list of installed packages
     # Choose location to save
     #   file.io is default, also enable gist and local file (.Rds or csv)
