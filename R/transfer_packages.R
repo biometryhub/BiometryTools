e119ba2c5888a34b6661942f18e1defb020b641e
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2020-10-03 14:23:03 +0930

    Updated transfer packages

--- R/transfer_packages.R
+++ R/transfer_packages.R
@@ -2,32 +2,29 @@
 #'
 #' @param library The location of the library on the current machine to copy.
 #' @param output One of `online` (the default), `gist` or `local`. Saves a list of installed packages to the chosen location, and provides instructions on how to use this to (re)install the packages elsewhere. See Details for more information.
-#' @param expiry Expiry for online file store. Ignored for other save locations.
+#' @param expiry Expiry for online file store in days. Weeks can be given with `w`, or months with `m`. Default is 7 days. Will be ignored if `output` is not `online`.
 #' @param filename Filename for the local output file. Ignored if `output` is not set to `local`.
+#' @param include_remotes Logical (default `TRUE`). Check for any packages installed from repositories other than CRAN, and output instructions to reinstall.
 #' @param quiet Logical (default `FALSE`). Suppress output if `TRUE`.
 #'
 #' @importFrom httr POST content
-#' @importFrom crayon green
+#' @importFrom crayon green blue
 #' @importFrom glue glue glue_col glue_collapse single_quote
 #'
 #' @details If `output` is `online`, the resulting list of currently installed packages is stored on [https://file.io](https://file.io) for the time specified in `expiry`.
-#' If `output` is `local`
+#' If `output` is `local`, an R script file (`.R`) is saved to the current working directory, which can be transferred manually to another machine.
 #'
 #' @return
 #' @export
 #'
 #' @examples
 #'
-transfer_packages <- function(library = .libPaths()[1], output = "online", expiry="14d", filename = "transfer_packages", quiet = F) {
-    # Get list of installed packages
-    # Choose location to save
-    #   file.io is default, also enable gist and local file (.Rds or csv)
-    # Upload, create gist or save file, then output instructions for other computer
-    # Also output those packages not installed from CRAN
+transfer_packages <- function(library = .libPaths()[1], output = "online", expiry="7d", filename = "transfer_packages", include_remotes = TRUE, quiet = FALSE) {
 
     pkgs <- unname(installed.packages(lib.loc = library, priority = "NA")[,1])
     to_install <- glue::glue("install.packages(c({glue::glue_collapse(glue::single_quote(pkgs), sep = ', ')}), repos = 'https://cloud.r-project.org')")
     green <- crayon::green
+    blue <- crayon::blue
 
     if(output == "online") {
         r <- httr::POST(glue::glue("https://file.io?expires={expiry}"), body = list(text = to_install))
@@ -48,5 +45,21 @@ transfer_packages <- function(library = .libPaths()[1], output = "online", expir
     }
     else {
         stop("output should be one of 'online', 'local' or 'gist'")
+    }
+
+    if(include_github) {
+        gh <- gh_packages(pkgs)
+        if(nrow(gh)>0 & !quiet) {
+            message(glue::glue_col("You have some packages installed from remote sources other than CRAN.
+                               They are: {blue {glue::glue_collapse(gh$repo, sep = ', ', last = ' and ')}}"))
+            # print(gh, row.names = F)
+            message(glue::glue_col("You will need to use the {blue remotes} package to reinstall them."))
+            if("github" %in% gh$type) {
+                message("To install those from github, run:")
+                output <- paste0("'", gh[which(gh$type=="github"),2], "/", gh[which(gh$type=="github"),3], "'", collapse = ", ")
+                message(glue::glue_col("To install those from github, run:
+                               {blue remotes::install_github(c({output}))}"))
+            }
+        }
     }
 }
