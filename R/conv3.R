ce07cfd0dd3af77de2af679d429591331e000016
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2020-10-21 10:16:18 +1030

    Auto alignment of some code

--- R/conv3.R
+++ R/conv3.R
@@ -13,60 +13,60 @@
 #'
 conv <- function(model, Env = "TSite:Variety", levs = NULL, sep = "-", ...){
   if(is.null(levs))
-      stop("Treatment levels cannnot be NULL.")
+    stop("Treatment levels cannnot be NULL.")
   evnam <- unlist(strsplit(Env, ":"))
   enam <- evnam[1]; vnam <- evnam[2]
   rterm <- attr(terms.formula(model$call$random), "term.labels")
   rterm <- rterm[grep(paste(evnam, collapse = "|"), rterm)]
   if(substring(rterm, 1, 2) == "fa"){
-      sumfa <- fa.asreml(model, trunc.char = NULL)
-      pvals <- sumfa$blups[[rterm]]$blups[,1:3]
-      Gmat <- sumfa$gammas[[rterm]]$Gmat
+    sumfa <- fa.asreml(model, trunc.char = NULL)
+    pvals <- sumfa$blups[[rterm]]$blups[,1:3]
+    Gmat <- sumfa$gammas[[rterm]]$Gmat
   }
   else {
-      pred <- predict(model, classify = Env, only = Env, ...)
-      Gmat <- summary(model, vparameters = TRUE)$vparameters[[Env]]
-      pvals <- pred$pvals
-      names(pvals)[3] <- "blup"
+    pred <- predict(model, classify = Env, only = Env, ...)
+    Gmat <- summary(model, vparameters = TRUE)$vparameters[[Env]]
+    pvals <- pred$pvals
+    names(pvals)[3] <- "blup"
   }
   tsnams <- dimnames(Gmat)[[2]]
   if(!length(grep(sep, tsnams)))
-      stop("Separator not found in Treatment by Site factor.")
+    stop("Separator not found in Treatment by Site factor.")
   st <- strsplit(tsnams, split = sep)
   tnam <- sapply(st, function(el) el[1])
   snam <- sapply(st, function(el) el[2])
   if(!all(levs %in% c(snam, tnam)))
-      stop("Treatment levels do not exist in ", enam)
+    stop("Treatment levels do not exist in ", enam)
   if(all(levs %in% snam)){
-      tnam <- snam
-      snam <- sapply(st, function(el) el[1])
+    tnam <- snam
+    snam <- sapply(st, function(el) el[1])
   }
   usnams <- unique(snam)
   tmat <- diag(nrow(Gmat))
   beta <- sigr <- c()
   blist <- list()
   for(i in 1:length(usnams)){
-      inds <- (1:length(snam))[snam %in% usnams[i]]
-      names(inds) <- tnam[inds]
-      whl <- (1:2)[levs %in% names(inds)]
-      if(length(whl) == 2){
-          tind <- inds[levs]
-          mat <- Gmat[tind, tind]
-          beta[i] <- mat[1,2]/mat[1,1]
-          rho <- mat[1,2]/sqrt(mat[1,1]*mat[2,2])
-          sigr[i] <- (mat[2,2]*(1 - rho^2))
-          tmat[tind[2], tind[1]] <- - beta[i]
-          blow <- pvals$blup[pvals[[enam]] %in% tsnams[tind[1]]]
-          bhigh <- pvals$blup[pvals[[enam]] %in% tsnams[tind[2]]]
-          bresp <- bhigh - beta[i]*blow
-          blist[[i]] <- cbind.data.frame(blow, bhigh, bresp)
-      } else {
-          slevs <- levs[whl]
-          tind <- inds[slevs]
-          if(whl == 1)
-              blist[[i]] <- cbind.data.frame(blow = pvals$blup[pvals[[enam]] %in% tsnams[tind]], bhigh = NA, bresp = NA)
-          else blist[[i]] <- cbind.data.frame(blow = NA, bhigh = pvals$blup[pvals[[enam]] %in% tsnams[tind]], bresp = NA)
-      }
+    inds <- (1:length(snam))[snam %in% usnams[i]]
+    names(inds) <- tnam[inds]
+    whl <- (1:2)[levs %in% names(inds)]
+    if(length(whl) == 2){
+      tind <- inds[levs]
+      mat <- Gmat[tind, tind]
+      beta[i] <- mat[1,2]/mat[1,1]
+      rho <- mat[1,2]/sqrt(mat[1,1]*mat[2,2])
+      sigr[i] <- (mat[2,2]*(1 - rho^2))
+      tmat[tind[2], tind[1]] <- - beta[i]
+      blow <- pvals$blup[pvals[[enam]] %in% tsnams[tind[1]]]
+      bhigh <- pvals$blup[pvals[[enam]] %in% tsnams[tind[2]]]
+      bresp <- bhigh - beta[i]*blow
+      blist[[i]] <- cbind.data.frame(blow, bhigh, bresp)
+    } else {
+      slevs <- levs[whl]
+      tind <- inds[slevs]
+      if(whl == 1)
+        blist[[i]] <- cbind.data.frame(blow = pvals$blup[pvals[[enam]] %in% tsnams[tind]], bhigh = NA, bresp = NA)
+      else blist[[i]] <- cbind.data.frame(blow = NA, bhigh = pvals$blup[pvals[[enam]] %in% tsnams[tind]], bresp = NA)
+    }
   }
   TGmat <- tmat %*% Gmat %*% t(tmat)
   tsnams <- gsub(levs[2], "resp", tsnams)
--- R/gh_packages.R
+++ R/gh_packages.R
@@ -1,23 +1,19 @@
 #' Checks if a package was installed from GitHub
 #'
-#' @param pkg A package to check
+#' @param pkg A character vector of package names to check.
 #'
 #' @return A dataframe containing packages installed from remote sources, with the type of remote, account name and repo name.
 #' @keywords internal
 #'
 gh_packages <- function(pkg) {
-  list <- which(sapply(pkg, function(x) !is.null(packageDescription(x)$RemoteType)))
-  results <- data.frame()
-  if (length(list) > 0) {
-    type <- sapply(pkg[list], function(x) packageDescription(x)$RemoteType)
-    username <- sapply(pkg[list], function(x) ifelse(is.null(packageDescription(x)$RemoteUsername), NA, packageDescription(x)$RemoteUsername))
-    repo <- names(list)
-    results <- data.frame(type, account = username, repo)
-    rownames(results) <- NULL
-  }
-  return(results)
+    list <- which(sapply(pkg, function(x) !is.null(packageDescription(x)$RemoteType)))
+    results <- data.frame()
+    if (length(list) > 0) {
+        type <- sapply(pkg[list], function(x) packageDescription(x)$RemoteType)
+        username <- sapply(pkg[list], function(x) ifelse(is.null(packageDescription(x)$RemoteUsername), NA, packageDescription(x)$RemoteUsername))
+        repo <- names(list)
+        results <- data.frame(type, account = username, repo)
+        rownames(results) <- NULL
+    }
+    return(results)
 }
-
-
-# Downloading Bioconductor repo https://git.bioconductor.org/packages/ACE
-# Installing 25 packages: zlibbioc, IRanges, S4Vectors, BiocGenerics, GenomeInfoDbData, futile.options, lambda.r, futile.logger, Rhtslib, BiocParallel, XVector, Biostrings, GenomicRanges, GenomeInfoDb, limma, marray, snowfall, CGHbase, Biobase, DNAcopy, impute, future.apply, Rsamtools, CGHcall, QDNAseq
